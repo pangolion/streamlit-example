d3f914c54ec5afbd9483e48390ac0ea5c68857e4
pangolion <72404372+pangolion@users.noreply.github.com>
2022-02-07 17:14:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import requests
 st.title("This is my first app")
 st.write("hellow word")
 st.info("This is my first homework")
+
+data = '{"model":"default"}'
+response = requests.post('http://colormind.io/api/', data=data)
+response.json()
+
+{'result': [[71, 94, 97], [109, 152, 137], [183, 213, 193], [243, 239, 204], [240, 153, 122]]}
