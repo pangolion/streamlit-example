9f47b2a2c24965b625f1d6acecb3c0885ce1f32d
pangolion <72404372+pangolion@users.noreply.github.com>
2022-02-07 17:41:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,9 @@ response = requests.post('http://colormind.io/api/', data=data)
 response.json()
 
 st.title("This is my first app")
-st.write("hellow word", color= '[49,47,49]')
+st.write("hellow word")
 st.info("This is my first homework")
+if st.button('change color')
+  st.markdown('color change', color= '[49,47,49]')
+else
+  st.markdown('changed', color= '[91,83,81]')
