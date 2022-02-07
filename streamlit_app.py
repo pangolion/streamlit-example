69001b3233a2b907d919bd53dc2c14f6bb6fb472
pangolion <72404372+pangolion@users.noreply.github.com>
2022-02-07 17:42:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ response.json()
 st.title("This is my first app")
 st.write("hellow word")
 st.info("This is my first homework")
-if st.button('change color')
+if st.button('change color'):
   st.markdown('color change', color= '[49,47,49]')
-else
+else:
   st.markdown('changed', color= '[91,83,81]')
