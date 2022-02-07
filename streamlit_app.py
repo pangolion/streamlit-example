e329f793c7c020f192c637eba1d76207dd59d4a8
pangolion <72404372+pangolion@users.noreply.github.com>
2022-02-07 18:17:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,10 @@ data = '{"model":"default"}'
 response = requests.post('http://colormind.io/api/', data=data)
 color_list = response.json()
 st.write(color_list['result'][1][1])
-
-c = '<p style="font-family:sans-serif; color:rgb(214,78,69); font-size: 42px;"> color</p>'
+R = color_list['result'][0][0]
+G = color_list['result'][0][1]
+B = color_list['result'][0][2]
+c = '<p style="font-family:sans-serif; color:rgb(R,G,B); font-size: 42px;"> color</p>'
 st.title("This is my first app")
 st.write("hellow word")
 st.info("This is my first homework")
