565cf1d7e1906ecff3689ac248608379dc1d8bb8
pangolion <72404372+pangolion@users.noreply.github.com>
2022-02-07 17:11:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,19 @@ import streamlit as st
 st.title("This is my first app")
 st.write("hellow word")
 st.info("This is my first homework")
+var url = "http://colormind.io/api/";
+var data = {
+	model : "default",
+	input : [[44,43,44],[90,83,82],"N","N","N"]
+}
+
+var http = new XMLHttpRequest();
+
+http.onreadystatechange = function() {
+	if(http.readyState == 4 && http.status == 200) {
+		var palette = JSON.parse(http.responseText).result;
+	}
+}
+
+http.open("POST", url, true);
+http.send(JSON.stringify(data));
