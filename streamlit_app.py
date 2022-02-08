686fc4943bd6f6e92145697f868c9479d4093c29
pangolion <72404372+pangolion@users.noreply.github.com>
2022-02-07 19:01:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,9 @@ st.write(color_list['result'][1][1])
 R = color_list['result'][0][0]
 G = color_list['result'][0][1]
 B = color_list['result'][0][2]
-exec("style="font-family:sans-serif; color:rgb(%d,%d,%d); font-size: 42px; %(R,G,B)")
-
+c = '<p style="font-family:sans-serif; color:rgb(%d,%d,%d); font-size: 42px;">New image</p>'
 st.title("This is my first app")
 st.write("hellow word")
 st.info("This is my first homework")
 if st.button('change color'):
- 
+ st.markdown(<p style="font-family:sans-serif; color:rgb(1,2,3); font-size: 42px;">New image</p>)
