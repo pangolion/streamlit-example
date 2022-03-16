165cbcc09b2a3897681e79964cd1ea5e63afe05e
pangolion <dh734@cornell.edu>
2022-03-16 01:37:16 -0400

    data

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,5 @@ import pandas as pd
 import streamlit as st
 import requests
 
-data = '{"model":"default"}'
-response = requests.post('http://colormind.io/api/', data=data)
-color_list = response.json()
-st.write(color_list['result'][1][1])
-R = color_list['result'][0][0]
-G = color_list['result'][0][1]
-B = color_list['result'][0][2]
-c = '<p style="font-family:sans-serif; color:rgb(%d,%d,%d); font-size: 42px;">New image</p>'
-st.title("This is my first app")
-st.write("hellow word")
-st.info("This is my first homework")
-if st.button('change color'):
- st.markdown('<p style="font-family:sans-serif; color:rgb(%d,%d,%d); font-size: 42px;">color changed</p>' %(R,G,B),unsafe_allow_html=True)
+data = pd.read_csv(r"C:\Users\wongt\OneDrive\桌面\SYS5160\archive\DataCoSupplyChainDataset.csv")
+ 
