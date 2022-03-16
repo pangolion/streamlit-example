2253addb86a2dc46ad0dbc49e58d419a4ef844b7
pangolion <72404372+pangolion@users.noreply.github.com>
2022-03-16 01:40:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import pandas as pd
 import streamlit as st
 import requests
 
-data = pd.read_csv(r"C:\Users\wongt\OneDrive\桌面\SYS5160\archive\DataCoSupplyChainDataset.csv")
+data = pd.read_csv(r"DataCoSupplyChainDataset.csv")
  
